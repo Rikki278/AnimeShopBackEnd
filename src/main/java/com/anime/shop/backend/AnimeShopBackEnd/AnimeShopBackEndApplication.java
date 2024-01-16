806365edbc68983b4ff993b747c3ca3ccc4568e8
RIKKI <jacobfryu@gmail.cpm>
2024-01-16 17:13:18 +0200

    Initial commit

---
+++ src/main/java/com/anime/shop/backend/AnimeShopBackEnd/AnimeShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.anime.shop.backend.AnimeShopBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeShopBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anime/shop/backend/AnimeShopBackEnd/AnimeShopBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anime.shop.backend.AnimeShopBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeShopBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
